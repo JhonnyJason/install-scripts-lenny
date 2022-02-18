82b7ceb23088cd7c733338b701323e632d0d69d6
Lenard Frommelt <lenny09918050@gmail.com>
2022-02-18 11:51:40 +0100

    produced updated meilisearchBaseConfig Script

--- fragments/caa.sh
+++ fragments/caa.sh
@@ -1,8 +1,9 @@
 pacman -Syu meilisearch --noconfirm
-#get 
+# get IP addresses 
 ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 ip6=$(/sbin/ip -o -6 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+# configure IP address to listen to
 sed -i 's/#MEILI_HTTP_ADDR=/MEILI_HTTP_ADDR='$ip4':{{{meiliport}}}/g' /etc/meilisearch.conf
-
+# kick off the service to run
 systemctl enable meilisearch
 systemctl start meilisearch
--- scripts/meilisearchBaseConfig.sh
+++ scripts/meilisearchBaseConfig.sh
@@ -7,5 +7,11 @@ echo 'KEYMAP=de-latin1' > /etc/vconsole.conf
 echo 'FONT=lat9w-16' >> /etc/vconsole.conf
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 pacman -Syu meilisearch --noconfirm
+# get IP addresses 
+ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+ip6=$(/sbin/ip -o -6 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+# configure IP address to listen to
+sed -i 's/#MEILI_HTTP_ADDR=/MEILI_HTTP_ADDR='$ip4':7700/g' /etc/meilisearch.conf
+# kick off the service to run
 systemctl enable meilisearch
 systemctl start meilisearch
